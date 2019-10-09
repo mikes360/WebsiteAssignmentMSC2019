0ce5af5094aedfec28c88537f53377ee49ac12c3
Michael Knight <mikes360@hotmail.com>
2019-10-09 22:07:17 +0100

    Code to create full model by replacing team id in the matches array with the team objects to the names and image paths can be used in rendering the view of the current game to bet on.

--- controller.js
+++ controller.js
@@ -37,7 +37,43 @@ async function getTeams(app)
 
 async function getGame(app)
 {
-    return app.set(DB_ALIAS).collection('game').find({}).toArray()
+    let game = await app.set(DB_ALIAS).collection('game').find({}).toArray()
+    let teams = await getTeams(app)
+
+    //let bob = game[0].matches
+    //let bob2 = bob[0]
+
+    
+
+    for(var i = 0; i < game[0].matches.length; i++) {
+        game[0].matches[i][0] = getTeam(teams, game[0].matches[i][0])
+        game[0].matches[i][1] = getTeam(teams, game[0].matches[i][1])
+    }
+
+    /*for(var match in game[0].matches){
+        match[0] = getTeam(teams, match[0])
+        match[1] = getTeam(teams, match[1])
+    }*/
+    
+    return game
+    // getTeams()
+    // getGames()
+
+    // foreach(match in game.matches)
+    // getTeam with id === match[0] id and match[1] id
+    // replace id string with team object
+    //return app.set(DB_ALIAS).collection('game').find({}).toArray()
 }
+
+function getTeam(teams, id) {
+    for(var i = 0; i < teams.length; i++) {
+        if(teams[i].teamID === id){
+            return teams[i];
+        }
+
+    }
+
+}
+
  
 module.exports = { connect, getUsers, getUser, addUser, getTeams, getGame }; 
